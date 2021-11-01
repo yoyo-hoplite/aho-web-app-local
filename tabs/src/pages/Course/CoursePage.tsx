2a52dfee53ac00079b9134c5b5bfeb27762722e9
Yoyo Pan <yoyo.pan@aimfolds.com>
2021-11-01 11:43:08 +0800

    aho-get-course api integration test

--- tabs/src/pages/Course/CoursePage.tsx
+++ tabs/src/pages/Course/CoursePage.tsx
@@ -2,19 +2,40 @@ import Banner, { useBannerHeight } from '../../components/Banner'
 import { Box } from '@mui/system'
 import { infoSx, titleSx, contentSx } from './styles'
 import { useEffect, useState } from 'react'
-import { FAKE_COURSES } from './fakedata'
 import CourseList from './CourseList'
 import CourseHistory from './CourseHistory'
 import CourseBadges from './CourseBadges'
+import axios from 'axios'
+import qs from 'qs'
 
 export const CoursePage = () => {
-  const [courses, setCourses] = useState(FAKE_COURSES)
+  // TODO: course type
+  const [courses, setCourses] = useState<any[]>([])
   const { bannerHeight } = useBannerHeight()
   const [contentHeight, setContentHeight] = useState<string>(`calc(100% - ${bannerHeight}px)`)
 
   useEffect(() => {
     setContentHeight(`calc(100% - ${bannerHeight}px)`)
   }, [bannerHeight])
+
+  useEffect(() => {
+    // NOTE: API works only at production site due to CORS issue
+    ;(async () => {
+      const params = {
+        courseId: 1,
+      }
+      const result = await axios({
+        method: 'POST',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        url: 'https://api.hoplite.info/aho_functions_internal_staging/aho-get-course',
+        data: qs.stringify(params),
+      })
+      // Debug msg
+      console.log(result.data)
+
+      setCourses([result.data.course])
+    })()
+  }, [])
 
   return (
     <main style={{ height: '100%' }}>
